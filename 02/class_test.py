12f7231ca224c95c4d97e22667876e3742644614
denis <den.degtyarev.00@gmail.com>
2023-10-07 18:22:40 +0300

    Done 2 homework

---
+++ 02/class_test.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import patch
+import main
+
+class TestMyFunctionCallCount(unittest.TestCase):
+    
+    @patch('main.parse_json')
+    def test_my_function_call_count(self, mock_function):
+        main.param = 4
+        main.main(3)
+        main.param = 3
+        main.main(2)
+        main.param = 5
+        main.main(10)
+        self.assertEqual(mock_function.call_count, 15)
+    
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 02/main.py
@@ -0,0 +1,53 @@
+'''
+Файл для 2 дз
+'''
+
+import json
+import time
+import json_gen
+
+def mean(k):
+    '''
+    Декоратор для функции, которая выводит среднее по времени среди k последних вызовов
+    '''
+    avg_time_k = []
+    avg_time_k_sum = 0
+    def _mean(func):
+        def wrapper(*args, **kwargs):
+            nonlocal avg_time_k
+            nonlocal avg_time_k_sum
+            start_time = time.time()
+            res = func(*args, **kwargs)
+            end_time = time.time()
+            time_taken = end_time - start_time
+            avg_time_k.append(time_taken)
+            avg_time_k_sum += time_taken
+            if len(avg_time_k) > k:
+                avg_time_k_sum -= avg_time_k[0]
+                avg_time_k.pop(0)
+            print(avg_time_k_sum / len(avg_time_k))
+            return res
+        return wrapper
+    return _mean
+
+PARAM = 10
+@mean(PARAM)
+def parse_json(json_str: str, keyword_callback, required_fields=None, keywords=None):
+    '''
+    Парсер json
+    '''
+    json_doc = json.loads(json_str)
+    if keywords in json_doc.keys():
+        lst = [str(keyword_callback(word)) if word == required_fields\
+                else word for word in json_doc[keywords].split()]
+        json_doc[keywords] = ' '.join(lst)
+    return json_doc
+
+
+def main(count_iter):
+    '''
+    main function for tests
+    '''
+    for _ in range(count_iter):
+        json_string, key, req_fields = json_gen.generate()
+        __ = parse_json(json_string, len, req_fields, key)
