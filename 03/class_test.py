68c1e8ebde88d66c1db7e4261bcc98e383d102ce
CHIS08 <den.degtyarev.00@gmail.com>
2023-10-26 14:38:25 +0300

    Add 3 lab

---
+++ 03/class_test.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest.mock import patch
+import main
+
+class TestCustomListOperations(unittest.TestCase):
+    def setUp(self):
+        self.lst1 = main.lst1
+        self.lst2 = main.lst2
+
+    def test_addition(self):
+        result = self.lst1 + self.lst2
+        self.assertEqual(result, main.CustomList([6, 6, 9, 4, 5]))
+        result = self.lst1 + [10,3,4,1]
+        self.assertEqual(result, main.CustomList([11, 5, 7, 5, 5]))
+        result = [10,3,4,1] + self.lst1
+        self.assertEqual(result, main.CustomList([11, 5, 7, 5, 5]))
+
+    def test_subtraction(self):
+        result = self.lst1 - self.lst2
+        self.assertEqual(result, main.CustomList([-4, -2, -3, 4, 5]))
+        result = self.lst2 - self.lst1
+        self.assertEqual(result, main.CustomList([4, 2, 3, -4, -5]))
+
+    def test_comparison(self):
+        self.assertFalse(self.lst1 < self.lst2)
+        self.assertTrue(self.lst1 <= self.lst2)
+        self.assertTrue(self.lst1 == self.lst2)
+        self.assertFalse(self.lst1 != self.lst2)
+        self.assertFalse(self.lst1 > self.lst2)
+        self.assertTrue(self.lst1 >= self.lst2)
+        print(self.lst1, self.lst2, sep='\n')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ 03/main.py
@@ -0,0 +1,93 @@
+'''
+Файл для 2 дз
+'''
+
+class CustomList(list):
+    """Updating list"""
+    def __init__(self, lst):
+        """Initialized"""
+        super().__init__()
+        self.lst = lst
+    def __add__(self, other):
+        """+"""
+        if isinstance(other, CustomList):
+            app1_list = other.lst.copy()
+        else:
+            app1_list = other.copy()
+        app2_list = self.lst.copy()
+        if len(app1_list) < len(app2_list):
+            app1_list += [0 for i in range(-len(app1_list) + len(app2_list))]
+        if len(app1_list) > len(app2_list):
+            app1_list += [0 for i in range(len(app1_list) - len(app2_list))]
+        return CustomList([app1_list[i] + app2_list[i] for i in range(len(app1_list))])
+    def __radd__(self, other):
+        """+"""
+        app1_list = other.copy()
+        app2_list = self.lst.copy()
+        if len(app1_list) < len(app2_list):
+            app1_list += [0 for i in range(-len(app1_list) + len(app2_list))]
+        if len(app1_list) > len(app2_list):
+            app1_list += [0 for i in range(len(app1_list) - len(app2_list))]
+        return CustomList([app1_list[i] + app2_list[i] for i in range(len(app1_list))])
+    def __sub__(self, other):
+        """-"""
+        if isinstance(other, CustomList):
+            app1_list = other.lst.copy()
+        else:
+            app1_list = other.copy()
+        app2_list = self.lst.copy()
+        if len(app1_list) < len(app2_list):
+            app1_list += [0] * (len(app2_list) - len(app1_list))
+        if len(app1_list) > len(app2_list):
+            app2_list += [0] * (len(app1_list) - len(app2_list))
+        return CustomList([app2_list[i] - app1_list[i] for i in range(len(app1_list))])
+
+    def __rsub__(self, other):
+        """-"""
+        app1_list = other.copy()
+        app2_list = self.lst.copy()
+        if len(app1_list) < len(app2_list):
+            app1_list += [0 for i in range(-len(app1_list) + len(app2_list))]
+        if len(app1_list) > len(app2_list):
+            app2_list += [0 for i in range(len(app1_list) - len(app2_list))]
+        return CustomList([app1_list[i] - app2_list[i] for i in range(len(app1_list))])
+    def __lt__(self, other):
+        """<"""
+        if isinstance(other, CustomList):
+            return sum(self.lst) < sum(other.lst)
+        return NotImplemented
+
+    def __le__(self, other):
+        """<="""
+        if isinstance(other, CustomList):
+            return sum(self.lst) <= sum(other.lst)
+        return NotImplemented
+
+    def __eq__(self, other):
+        """=="""
+        if isinstance(other, CustomList):
+            return sum(self.lst) == sum(other.lst)
+        return NotImplemented
+
+    def __ne__(self, other):
+        """!="""
+        if isinstance(other, CustomList):
+            return sum(self.lst) != sum(other.lst)
+        return NotImplemented
+
+    def __gt__(self, other):
+        """>"""
+        if isinstance(other, CustomList):
+            return sum(self.lst) > sum(other.lst)
+        return NotImplemented
+
+    def __ge__(self, other):
+        """>="""
+        if isinstance(other, CustomList):
+            return sum(self.lst) >= sum(other.lst)
+        return NotImplemented
+    def __str__(self):
+        return f"CustomList: {self.lst}, sum: {sum(self.lst)}"
+
+lst1 = CustomList([1,2,3,4,5])
+lst2 = CustomList([5,4,6])
